259da82171b2547bc7feaf68e0fc1e85e8e322a5
Manish Roy <manish.11612939@lpu.in>
2023-02-22 19:13:42 +0530

    integrated components to App

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
+// Components
+import Navigation from './components/Navigation'
+import Search from './components/Search'
+import Domain from './components/Domain'
+
+// ABIs
+import ETHDaddy from './abis/ETHDaddy.json'
+
+// Config
+import config from './config.json';
+
+function App() {
+  const [ provider, setProvider ] = useState(null)
+  const [ account, setAccount ] = useState(null)
+  const [ ethDaddy, setEthDaddy ] = useState(null)
+  const [ domains, setDomains ] = useState([])
+
+
+  const fetchBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+    const ethDaddy = new ethers.Contract(config[network.chainId].ETHDaddy.address, ETHDaddy, provider)
+    setEthDaddy(ethDaddy)
+
+    const maxSupply = await ethDaddy.maxSupply()
+    const domains = []
+
+    for (let i = 1; i <= maxSupply; i++) {
+      const domain = await ethDaddy.getDomain(i)
+      domains.push(domain)
+    }
+
+    setDomains(domains)
+  }
+
+  useEffect(() => {
+    account && fetchBlockchainData()
+  }, [ account ])
+
+  useEffect(() => {
+     window.ethereum.on("accountsChanged", async () => {
+      const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account)
+    })
+  }, [])
+ 
+
+  return (
+    <div>
+      <Navigation account={ account } setAccount={ setAccount } />
+      <Search />
+      <div className='cards__section'>
+
+        <h2 className='cards__title'>Why you need a domain name?</h2>
+        <p className='cards__description'>
+          Own your custom domain username, use it across services, and be able to store an avatar and other profile data.
+        </p>
+        <hr />
+
+        <div className='cards'>
+          {
+            domains.map((domain, idx) => (
+              <Domain domain={ domain } ethDaddy={ ethDaddy } provider={  provider } id={ idx + 1 } key={ idx } />
+            ))
+          }
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
